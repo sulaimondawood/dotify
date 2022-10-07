179cc7376702a7769633dc026d7c3d1c4085b4d2
sulaimondawood <sulaimondawod@gmail.com>
2022-10-07 12:39:48 +0100

    delted all pages
    starting from scrash

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,24 +1,5 @@
-import styles from "../styles/page/sidebar.module.css";
+import React from "react";
 
-import SideBarContent from "./SideBarContent";
-import { FaPlayCircle } from "react-icons/fa";
-import { FiSearch } from "react-icons/fi";
-import { GrHomeRounded } from "react-icons/gr";
-const SideBar = () => {
-  return (
-    <aside className={styles.aside}>
-      <div className={styles.logo}>
-        DO <span className={styles.logo_text}>tify</span>
-      </div>
-      <input type="text" className={styles.search} />
-      <div className={styles.left}>
-        <SideBarContent route="/" home="Home" homeIcon={<FiSearch />} />
-        <SideBarContent route="/" home="Previous" homeIcon={<FiSearch />} />
-        <SideBarContent route="/" home="Top PlayList" homeIcon={<FiSearch />} />
-        <SideBarContent route="/" home="Home" homeIcon={<FiSearch />} />
-      </div>
-    </aside>
-  );
+export const SideBar = () => {
+  return <div>SideBar</div>;
 };
-
-export default SideBar;
